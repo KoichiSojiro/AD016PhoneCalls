5df4b3207c78dca795f41ba7f4668fca836a7128
Koichi <koichisojiro@live.com>
2016-09-18 15:12:51 +0700

    initial

---
+++ app/src/androidTest/java/com/example/trannh08/ad016phonecalls/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.trannh08.ad016phonecalls;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/example/trannh08/ad016phonecalls/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.trannh08.ad016phonecalls;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
